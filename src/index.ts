1ffacff75eed85ec5a04f6d5287cf751d2c1c7bc
Winston Liu <Wliu1402@gmail.com>
2021-08-16 23:13:04 -0400

    Janky UI

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,48 @@
 import { Solver } from './solver';
 
-const solutions = Solver.solve([2, 10, 10, 3]);
-for (const output of Solver.print(solutions)) {
-  console.log(output);
+(document.getElementById("solve") as HTMLButtonElement).addEventListener("click", solve);
+
+function solve() {
+  const cards = parse();
+
+  const solutionsDiv = document.getElementById("solutions") as HTMLDivElement;
+  while (solutionsDiv.lastElementChild) {
+    solutionsDiv.removeChild(solutionsDiv.lastElementChild);
+  }
+
+  const solutions = Solver.solve(cards);
+  if (solutions.length !== 0) {
+    const solutionElements = Solver.print(solutions).map(output => {
+      const solutionElement = document.createElement("pre");
+      solutionElement.innerText = output;
+      return solutionElement;
+    });
+    solutionsDiv.append(...solutionElements);
+  } else {
+    const noSolutionsElement = document.createElement("pre");
+    noSolutionsElement.innerText = "No solutions";
+    solutionsDiv.appendChild(noSolutionsElement);
+  }
+}
+
+function parse(): number[] {
+  const cards: number[] = [];
+  for (let i = 1; i <= 4; i++) {
+    const value = (document.getElementById(`card${i}`) as HTMLInputElement).value;
+    const number = Number(value);
+    if (number >= 1 && number <= 13) {
+      cards.push(number);
+    } else if (/^(j|jack)$/i.test(value)) {
+      cards.push(11);
+    } else if (/^(q|queen)$/i.test(value)) {
+      cards.push(12);
+    } else if (/^(k|king)$/i.test(value)) {
+      cards.push(13);
+    } else if (/^(a|ace)$/i.test(value)) {
+      cards.push(1);
+    } else {
+      alert(`Card ${i} is invalid`);
+    }
+  }
+  return cards;
 }
