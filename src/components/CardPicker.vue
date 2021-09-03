269406bd99a97b92982f511d739922662ceef26f
Winston Liu <Wliu1402@gmail.com>
2021-09-02 21:25:23 -0400

    Get dynamic card selection working again

--- src/components/CardPicker.vue
+++ src/components/CardPicker.vue
@@ -5,35 +5,77 @@
       <select
         :name="`card${id}`"
         :id="`card${id}`"
+        v-model="selected"
         class="card-picker form-select"
         required
       >
-        <option value="1">Ace</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-        <option value="10">10</option>
-        <option value="11">Jack</option>
-        <option value="12">Queen</option>
-        <option value="13">King</option>
+        <option v-for="card in cards" :value="card.value" :key="card.value">
+          {{ card.text }}
+        </option>
       </select>
     </div>
     <div class="card-image p-3">
-      <img src="" />
+      <img :src="image.src" :alt="image.alt" :title="image.title" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, watch } from "vue";
+
+interface Image {
+  src: string;
+  alt: string;
+  title: string;
+}
+
+const cards = [
+  { text: "Ace", value: "1" },
+  { text: "2", value: "2" },
+  { text: "3", value: "3" },
+  { text: "4", value: "4" },
+  { text: "5", value: "5" },
+  { text: "6", value: "6" },
+  { text: "7", value: "7" },
+  { text: "8", value: "8" },
+  { text: "9", value: "9" },
+  { text: "10", value: "10" },
+  { text: "Jack", value: "11" },
+  { text: "Queen", value: "12" },
+  { text: "King", value: "13" },
+];
+
+const faces = ["Clubs", "Diamonds", "Hearts", "Spades"];
+
 export default defineComponent({
+  setup() {
+    const selected = ref(1 + Math.floor(Math.random() * 13));
+
+    const updateImage = (): Image => {
+      const face = faces[Math.floor(Math.random() * 4)];
+      return {
+        src: `images/light/${selected.value}${face[0]}.svg`,
+        alt: `${cards[selected.value - 1].text} of ${face}`,
+        title: `${cards[selected.value - 1].text} of ${face}`,
+      };
+    };
+
+    const image = ref(updateImage());
+    watch(selected, () => {
+      image.value = updateImage();
+    });
+    return {
+      selected,
+      image,
+    };
+  },
   props: {
     id: Number,
+  },
+  data() {
+    return {
+      cards,
+    };
   },
 });
 </script>
