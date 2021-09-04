1360e7e6e84db429f341e1260ee2ddb39566d849
Winston Liu <Wliu1402@gmail.com>
2021-09-03 20:47:55 -0400

    Try an explicit relative path?

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 module.exports = {
-  publicPath: "", // Use relative paths (don't assume we're at the domain root)
+  publicPath: "./", // Use relative paths (don't assume we're at the domain root)
 };
