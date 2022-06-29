9c7524227e0b0489e03f840be489cbdd6912337c
Winston Liu <Wliu1402@gmail.com>
2022-06-29 05:21:59 +0000

    Remove @vue/eslint-config-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,8 +9,7 @@ module.exports = {
     "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
     ecmaVersion: 2021,
