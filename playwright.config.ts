a6d72aba645db8a727493c3766417ac1a81b2dee
Winston Liu <Wliu1402@gmail.com>
2023-04-15 23:26:12 -0700

    Debugging time

--- playwright.config.ts
+++ playwright.config.ts
@@ -65,7 +65,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run dev",
-    url: "http://127.0.0.1:5173",
+    port: 5173,
     reuseExistingServer: !process.env.CI,
   },
 });
