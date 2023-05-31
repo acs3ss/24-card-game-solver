43cfd889972b73bc29d5f9b6dcc8312f91b28351
Winston Liu <Wliu1402@gmail.com>
2023-05-30 22:55:17 -0700

    Switch to Bootstrap variables

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "./light.scss";
-import "./dark.scss";
+import "./main.scss";
+// import "./dark.scss";
 
 const app = createApp(App);
 app.mount("#app");
