0dc0b783dc6969ed2a544409f7b1aaf8f45de0f0
Winston Liu <Wliu1402@gmail.com>
2024-03-09 19:10:54 -0800

    Add priority tests

--- tests/unit/operation.test.ts
+++ tests/unit/operation.test.ts
@@ -13,11 +13,27 @@ describe("Operations", () => {
     test("Adds numbers together", () => {
       expect(Add.operate(2, 5)).toBe(2 + 5);
     });
+
+    test("Has the same priority as Subtract", () => {
+      expect(Add.priority).toBe(Subtract.priority);
+    });
+
+    test("Has a lower priority than Multiply", () => {
+      expect(Add.priority).toBeLessThan(Multiply.priority);
+    });
   });
 
   describe("Subtract", () => {
     test("Subtracts b from a", () => {
       expect(Subtract.operate(2, 5)).toBe(2 - 5);
+    });
+
+    test("Has the same priority as Add", () => {
+      expect(Subtract.priority).toBe(Add.priority);
+    });
+
+    test("Has a lower priority than Multiply", () => {
+      expect(Add.priority).toBeLessThan(Multiply.priority);
     });
   });
 
@@ -25,11 +41,35 @@ describe("Operations", () => {
     test("Multiplies numbers together", () => {
       expect(Multiply.operate(2, 5)).toBe(2 * 5);
     });
+
+    test("Has a higher priority than Add", () => {
+      expect(Multiply.priority).toBeGreaterThan(Add.priority);
+    });
+
+    test("Has the same priority as Divide", () => {
+      expect(Multiply.priority).toBe(Divide.priority);
+    });
+
+    test("Has a lower priority than Exponent", () => {
+      expect(Multiply.priority).toBeLessThan(Exponent.priority);
+    });
   });
 
   describe("Divide", () => {
     test("Divides a by b", () => {
       expect(Divide.operate(2, 5)).toBe(2 / 5);
+    });
+
+    test("Has a higher priority than Add", () => {
+      expect(Divide.priority).toBeGreaterThan(Add.priority);
+    });
+
+    test("Has the same priority as Multiply", () => {
+      expect(Divide.priority).toBe(Multiply.priority);
+    });
+
+    test("Has a lower priority than Exponent", () => {
+      expect(Divide.priority).toBeLessThan(Exponent.priority);
     });
   });
 
@@ -37,11 +77,27 @@ describe("Operations", () => {
     test("Raises a to the b power", () => {
       expect(Exponent.operate(2, 5)).toBe(2 ** 5);
     });
+
+    test("Has the same priority as Log", () => {
+      expect(Exponent.priority).toBe(Log.priority);
+    });
+
+    test("Has a higher priority than Multiply", () => {
+      expect(Exponent.priority).toBeGreaterThan(Multiply.priority);
+    });
   });
 
   describe("Log", () => {
     test("Performs log_a(b)", () => {
       expect(Log.operate(2, 8)).toBe(3);
+    });
+
+    test("Has the same priority as Exponent", () => {
+      expect(Log.priority).toBe(Exponent.priority);
+    });
+
+    test("Has a higher priority than Multiply", () => {
+      expect(Log.priority).toBeGreaterThan(Multiply.priority);
     });
   });
 });
