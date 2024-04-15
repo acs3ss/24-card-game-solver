983b712d5216de12342dc79441567123e7893914
Winston Liu <Wliu1402@gmail.com>
2024-04-15 09:45:41 -0700

    Switch trace collection to retain-on-first-failure

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,8 +22,8 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "http://localhost:5173",
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    /* Collect trace for failed tests. See https://playwright.dev/docs/trace-viewer */
+    trace: "retain-on-first-failure",
   },
 
   /* Configure projects for major browsers */
